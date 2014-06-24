440231f241776b63525008ecf8186e32414dbbf1
kiwi <jcwen@thoughtworks.com>
2014-06-24 13:13:32 +0800

    split InjectPointFinder method into multi small private inner class

--- src/main/java/org/kiwi/kjector/InjectPointFinder.java
+++ src/main/java/org/kiwi/kjector/InjectPointFinder.java
@@ -21,67 +21,114 @@ public class InjectPointFinder {
     }
 
     private InjectPoint getNonConstructorInjectPoint(Class klass, InjectPoint injectPoint) {
-        if (klass.getSuperclass() == Object.class) {
-            return getMethodInjectPoint(klass, getFieldInjectPoint(klass, injectPoint));
-        }
-        return getMethodInjectPoint(klass, getFieldInjectPoint(klass, getNonConstructorInjectPoint(klass.getSuperclass(), injectPoint)));
-    }
-
-    private InjectPoint getMethodInjectPoint(Class klass, InjectPoint injectPoint) {
-        final List<Method> methods = getMethodInjectPoint(klass);
-        return methods.isEmpty() ? injectPoint : new MethodInjectPoint(injectPoint, methods);
-    }
-
-    private List<Method> getMethodInjectPoint(Class klass) {
-        return Arrays.asList(klass.getDeclaredMethods()).stream()
-                .filter(method -> method.getAnnotation(Inject.class) != null)
-                .collect(Collectors.toList());
-    }
-
-    private InjectPoint getFieldInjectPoint(Class klass, InjectPoint injectPoint) {
-        final List<Field> fields = getFieldInjectPoint(klass);
-        return fields.isEmpty() ? injectPoint : new FieldInjectPoint(injectPoint, fields);
-    }
-
-    private List<Field> getFieldInjectPoint(Class klass) {
-        return Arrays.asList(klass.getDeclaredFields()).stream()
-                .filter(field -> field.getAnnotation(Inject.class) != null)
-                .collect(Collectors.toList());
+        return new NonConstructorInjectPointFinder(klass).findInjectPoint(injectPoint);
     }
 
     private InjectPoint getConstructorInjectPoint(Class klass) {
-        final List<Constructor> injectConstructors = getInjectConstructor(klass);
-        if (injectConstructors.size() > 1) {
-            throw new MultiConstructorInjectPointFoundException(klass);
-        }
-
-        if (!injectConstructors.isEmpty()) {
-            return new ParameterConstructorInjectPoint(injectConstructors.get(0));
-        }
-
-        final Constructor defaultConstructor = getDefaultConstructor(klass);
-        if (defaultConstructor == null) {
-            throw new InjectPointNotFoundException(klass);
-        }
-
-        return new DefaultConstructorInjectPoint(defaultConstructor);
+        return new ConstructorInjectPointFinder(klass).findInjectPoint();
     }
 
-    private List<Constructor> getInjectConstructor(Class klass) {
-        return Arrays.asList(klass.getConstructors()).stream()
-                .filter(this::isInjectConstructor)
-                .collect(Collectors.toList());
+    private class ConstructorInjectPointFinder {
+        private Class klass;
+
+        public ConstructorInjectPointFinder(Class klass) {
+            this.klass = klass;
+        }
+
+        public InjectPoint findInjectPoint() {
+            final List<Constructor> injectConstructors = getInjectConstructor();
+            if (injectConstructors.size() > 1) {
+                throw new MultiConstructorInjectPointFoundException(klass);
+            }
+
+            if (!injectConstructors.isEmpty()) {
+                return new ParameterConstructorInjectPoint(injectConstructors.get(0));
+            }
+
+            final Constructor defaultConstructor = getDefaultConstructor();
+            if (defaultConstructor == null) {
+                throw new InjectPointNotFoundException(klass);
+            }
+
+            return new DefaultConstructorInjectPoint(defaultConstructor);
+        }
+
+        private List<Constructor> getInjectConstructor() {
+            return Arrays.asList(klass.getConstructors()).stream()
+                    .filter(this::isInjectConstructor)
+                    .collect(Collectors.toList());
+        }
+
+        private boolean isInjectConstructor(Constructor constructor) {
+            return constructor.getAnnotation(Inject.class) != null;
+        }
+
+        private Constructor getDefaultConstructor() {
+            try {
+                return klass.getConstructor();
+            } catch (NoSuchMethodException e) {
+                return null;
+            }
+        }
     }
 
-    private boolean isInjectConstructor(Constructor constructor) {
-        return constructor.getAnnotation(Inject.class) != null;
+    private class NonConstructorInjectPointFinder {
+        private final Class klass;
+        private final MethodInjectPointFinder methodInjectPointFinder;
+        private final FieldInjectPointerFinder fieldInjectPointerFinder;
+
+        public NonConstructorInjectPointFinder(Class klass) {
+            this.klass = klass;
+            this.methodInjectPointFinder = new MethodInjectPointFinder(klass);
+            this.fieldInjectPointerFinder = new FieldInjectPointerFinder(klass);
+        }
+
+        public InjectPoint findInjectPoint(InjectPoint injectPoint) {
+            if (klass.getSuperclass() == Object.class) {
+                return methodInjectPointFinder.findInjectPoint(fieldInjectPointerFinder.findInjectPoint(injectPoint));
+            }
+            return methodInjectPointFinder.findInjectPoint(
+                    fieldInjectPointerFinder.findInjectPoint(
+                            new NonConstructorInjectPointFinder(klass.getSuperclass()).findInjectPoint(injectPoint)));
+        }
+
     }
 
-    private Constructor getDefaultConstructor(Class klass) {
-        try {
-            return klass.getConstructor();
-        } catch (NoSuchMethodException e) {
-            return null;
+    private class MethodInjectPointFinder {
+        private final Class klass;
+
+        public MethodInjectPointFinder(Class klass) {
+            this.klass = klass;
+        }
+
+        public InjectPoint findInjectPoint(InjectPoint injectPoint) {
+            final List<Method> methods = getMethodWithInjectAnnotation();
+            return methods.isEmpty() ? injectPoint : new MethodInjectPoint(injectPoint, methods);
+        }
+
+        private List<Method> getMethodWithInjectAnnotation() {
+            return Arrays.asList(klass.getDeclaredMethods()).stream()
+                    .filter(method -> method.getAnnotation(Inject.class) != null)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private class FieldInjectPointerFinder {
+        private final Class klass;
+
+        public FieldInjectPointerFinder(Class klass) {
+            this.klass = klass;
+        }
+
+        public InjectPoint findInjectPoint(InjectPoint injectPoint) {
+            final List<Field> fields = getFieldWithInjectAnnotation();
+            return fields.isEmpty() ? injectPoint : new FieldInjectPoint(injectPoint, fields);
+        }
+
+        private List<Field> getFieldWithInjectAnnotation() {
+            return Arrays.asList(klass.getDeclaredFields()).stream()
+                    .filter(field -> field.getAnnotation(Inject.class) != null)
+                    .collect(Collectors.toList());
         }
     }
 }
